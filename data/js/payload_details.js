1ce459ba1094e9dd93c66ae2d27a998d8815e909
mfshill <mh-signup@outlook.com>
2016-09-02 02:18:49 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33719,
-	"created_at": "Thu Sep 01 01:18:47 +0000 2016",
+	"tweets": 33722,
+	"created_at": "Fri Sep 02 01:18:49 +0000 2016",
 	"lang": "en"
 }
---
+++ data/js/tweets/2016_09.js
@@ -0,0 +1,558 @@
+Grailbird.data.tweets_2016_09 = 
+[
+	{
+		"created_at": "Thu Sep 01 21:16:16 +0000 2016",
+		"id": 771456664075640800,
+		"id_str": "771456664075640832",
+		"text": "Better late than never, this year the cactus 🌵 is heralding the end of summer rather then the… https://t.co/GZii15UIyL",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GZii15UIyL",
+					"expanded_url": "https://www.instagram.com/p/BJ0_vQ0go_J/",
+					"display_url": "instagram.com/p/BJ0_vQ0go_J/",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Mikey",
+			"screen_name": "mfshill",
+			"location": "#LondonTown",
+			"description": "endeavouring to keep the art of coquetry alive, oenophile, dance music fanatic.",
+			"url": "http://t.co/k4hFF5ZqJA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/k4hFF5ZqJA",
+							"expanded_url": "http://bit.ly/132cPfC",
+							"display_url": "bit.ly/132cPfC",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281,
+			"friends_count": 253,
+			"listed_count": 26,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 271,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33722,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1471806021",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Sep 01 19:48:09 +0000 2016",
+		"id": 771434488849113100,
+		"id_str": "771434488849113088",
+		"text": "@Jamie_Dodger_ 😐",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jamie_Dodger_",
+					"name": "Jamie_Dodger_",
+					"id": 27925847,
+					"id_str": "27925847",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 771426146898575400,
+		"in_reply_to_status_id_str": "771426146898575360",
+		"in_reply_to_user_id": 27925847,
+		"in_reply_to_user_id_str": "27925847",
+		"in_reply_to_screen_name": "Jamie_Dodger_",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Mikey",
+			"screen_name": "mfshill",
+			"location": "#LondonTown",
+			"description": "endeavouring to keep the art of coquetry alive, oenophile, dance music fanatic.",
+			"url": "http://t.co/k4hFF5ZqJA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/k4hFF5ZqJA",
+							"expanded_url": "http://bit.ly/132cPfC",
+							"display_url": "bit.ly/132cPfC",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281,
+			"friends_count": 253,
+			"listed_count": 26,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 271,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33722,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1471806021",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und"
+	},
+	{
+		"created_at": "Thu Sep 01 18:20:19 +0000 2016",
+		"id": 771412385429123100,
+		"id_str": "771412385429123073",
+		"text": "RT @clairejt: Sometimes pictures write their own captions https://t.co/sGGlJ5pgO5",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "clairejt",
+					"name": "Septic Peg",
+					"id": 17920735,
+					"id_str": "17920735",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 771324943783039000,
+					"id_str": "771324943783038976",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+					"url": "https://t.co/sGGlJ5pgO5",
+					"display_url": "pic.twitter.com/sGGlJ5pgO5",
+					"expanded_url": "https://twitter.com/clairejt/status/771324965136306176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 771324965136306200,
+					"source_status_id_str": "771324965136306176",
+					"source_user_id": 17920735,
+					"source_user_id_str": "17920735"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 771324943783039000,
+					"id_str": "771324943783038976",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+					"url": "https://t.co/sGGlJ5pgO5",
+					"display_url": "pic.twitter.com/sGGlJ5pgO5",
+					"expanded_url": "https://twitter.com/clairejt/status/771324965136306176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 571,
+							"h": 667,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 771324965136306200,
+					"source_status_id_str": "771324965136306176",
+					"source_user_id": 17920735,
+					"source_user_id_str": "17920735"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Mikey",
+			"screen_name": "mfshill",
+			"location": "#LondonTown",
+			"description": "endeavouring to keep the art of coquetry alive, oenophile, dance music fanatic.",
+			"url": "http://t.co/k4hFF5ZqJA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/k4hFF5ZqJA",
+							"expanded_url": "http://bit.ly/132cPfC",
+							"display_url": "bit.ly/132cPfC",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281,
+			"friends_count": 253,
+			"listed_count": 26,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 271,
+			"utc_offset": 3600,
+			"time_zone": "London",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33722,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000168323347/B9N3gVAB.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/726804552159088641/5pd-jNA1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1471806021",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Sep 01 12:32:56 +0000 2016",
+			"id": 771324965136306200,
+			"id_str": "771324965136306176",
+			"text": "Sometimes pictures write their own captions https://t.co/sGGlJ5pgO5",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 771324943783039000,
+						"id_str": "771324943783038976",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+						"url": "https://t.co/sGGlJ5pgO5",
+						"display_url": "pic.twitter.com/sGGlJ5pgO5",
+						"expanded_url": "https://twitter.com/clairejt/status/771324965136306176/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 771324943783039000,
+						"id_str": "771324943783038976",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/CrRL8yCWEAAhuXb.jpg",
+						"url": "https://t.co/sGGlJ5pgO5",
+						"display_url": "pic.twitter.com/sGGlJ5pgO5",
+						"expanded_url": "https://twitter.com/clairejt/status/771324965136306176/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 571,
+								"h": 667,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17920735,
+				"id_str": "17920735",
+				"name": "Septic Peg",
+				"screen_name": "clairejt",
+				"location": "Northern Ireland",
+				"description": "Just imagine an lol after each tweet & give it a good 24 hours before you make your mind up. It's not tinder & I will prob screen shot your DM for the craic.",
+				"url": "https://t.co/VUqTwJu6td",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VUqTwJu6td",
+								"expanded_url": "https://septicpeggy.wordpress.com/",
+								"display_url": "septicpeggy.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2566,
+				"friends_count": 897,
+				"listed_count": 37,
+				"created_at": "Sat Dec 06 12:03:05 +0000 2008",
+				"favourites_count": 65734,
+				"utc_offset": 3600,
+				"time_zone": "London",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 149493,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000111939902/69b50401ce28af2f67cbc6ad191aee92.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000111939902/69b50401ce28af2f67cbc6ad191aee92.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/752558995534118912/pOHWjudv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/752558995534118912/pOHWjudv_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17920735/1394892696",
+				"profile_link_color": "B9B9B9",
+				"profile_sidebar_border_color": "333333",
+				"profile_sidebar_fill_color": "0F0F0F",
+				"profile_text_color": "3FD85B",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 34,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
