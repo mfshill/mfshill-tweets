173f02130d6b0d2540336399522f80155b4d2166
mfshill <mh-signup@outlook.com>
2019-05-07 02:17:39 +0100

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 06 16:52:04 +0000 2019",
+		"id": 1125443122396377100,
+		"id_str": "1125443122396377088",
+		"full_text": "'so so true 'waiting for the but' via xkcd https://t.co/wscst9Y0lf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1125443111369490400,
+					"id_str": "1125443111369490432",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/D55gjAQWwAA4tnn.png",
+					"media_url_https": "https://pbs.twimg.com/media/D55gjAQWwAA4tnn.png",
+					"url": "https://t.co/wscst9Y0lf",
+					"display_url": "pic.twitter.com/wscst9Y0lf",
+					"expanded_url": "https://twitter.com/mfshill/status/1125443122396377088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1125443111369490400,
+					"id_str": "1125443111369490432",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/D55gjAQWwAA4tnn.png",
+					"media_url_https": "https://pbs.twimg.com/media/D55gjAQWwAA4tnn.png",
+					"url": "https://t.co/wscst9Y0lf",
+					"display_url": "pic.twitter.com/wscst9Y0lf",
+					"expanded_url": "https://twitter.com/mfshill/status/1125443122396377088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 290,
+							"h": 402,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "Thinker, runner, cyclist, oenophile, dance music fanatic.",
+			"url": "https://t.co/ZCrqLtp1Br",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZCrqLtp1Br",
+							"expanded_url": "https://cinehabitue.wordpress.com/",
+							"display_url": "cinehabitue.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 280,
+			"friends_count": 272,
+			"listed_count": 24,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 38989,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065997730772856832/H9GF04-N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065997730772856832/H9GF04-N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1542988698",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'so so true 'waiting for the but' via xkcd https://t.co/wscst9Y0lf"
+	},
+	{
 		"created_at": "Sun May 05 17:44:39 +0000 2019",
 		"id": 1125091169460006900,
 		"id_str": "1125093968314818560",
