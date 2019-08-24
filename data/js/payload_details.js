d6828e071778d340eac4b796e677eb1f29098ea3
mfshill <mh-signup@outlook.com>
2019-08-24 02:17:40 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 39685,
-	"created_at": "Thu Aug 22 01:17:38 +0000 2019",
+	"tweets": 39686,
+	"created_at": "Sat Aug 24 01:17:39 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 23 13:56:36 +0000 2019",
+		"id": 1164877419226030000,
+		"id_str": "1164899239195029505",
+		"full_text": "Emery to his defenders before kick-off https://t.co/Pi2jf9tfk6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1164877395444285400,
+					"id_str": "1164877395444285440",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1164877395444285440/pu/img/sXoq_h1-oOisJDeG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1164877395444285440/pu/img/sXoq_h1-oOisJDeG.jpg",
+					"url": "https://t.co/Pi2jf9tfk6",
+					"display_url": "pic.twitter.com/Pi2jf9tfk6",
+					"expanded_url": "https://twitter.com/SimpsonsArsenal/status/1164877419226030080/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1040,
+							"h": 780,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1040,
+							"h": 780,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1164877395444285400,
+					"id_str": "1164877395444285440",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1164877395444285440/pu/img/sXoq_h1-oOisJDeG.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1164877395444285440/pu/img/sXoq_h1-oOisJDeG.jpg",
+					"url": "https://t.co/Pi2jf9tfk6",
+					"display_url": "pic.twitter.com/Pi2jf9tfk6",
+					"expanded_url": "https://twitter.com/SimpsonsArsenal/status/1164877419226030080/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1040,
+							"h": 780,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1040,
+							"h": 780,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"duration_millis": 16800,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1164877395444285440/pu/pl/5MercWLktrYRX-95.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1164877395444285440/pu/vid/480x360/M4_gkDsOQdoxV5bl.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1164877395444285440/pu/vid/360x270/UL2EYYcoQarXY9ni.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1164877395444285440/pu/vid/960x720/k1FZQw5PPBGwgxv_.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1096703520798056400,
+			"id_str": "1096703520798056450",
+			"name": "Simpsons Arsenal",
+			"screen_name": "SimpsonsArsenal",
+			"location": "",
+			"description": "Life as an @Arsenal fan through Simpsons quotes and gifs",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14632,
+			"friends_count": 31,
+			"listed_count": 69,
+			"created_at": "Sat Feb 16 09:31:09 +0000 2019",
+			"favourites_count": 1186,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 857,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1161285080226127874/sqXr48eC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1161285080226127874/sqXr48eC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1096703520798056450/1559308826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 118,
+		"favorite_count": 567,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Emery to his defenders before kick-off https://t.co/Pi2jf9tfk6"
+	},
+	{
 		"created_at": "Wed Aug 21 21:42:58 +0000 2019",
 		"id": 1164186758646575000,
 		"id_str": "1164291831116566528",
