88a0561d13877d3cd6d43121e8a44c5a3446f37e
mfshill <mh-signup@outlook.com>
2020-03-25 01:17:40 +0000

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Tue Mar 24 22:01:24 +0000 2020",
+		"id": 1242561773921763300,
+		"id_str": "1242572246444642305",
+		"full_text": "Ladies &amp; gentlemen, we have a winner. She just gets better &amp; better. (nb : Bit o’ cussin if there’s a child nearby) https://t.co/m9QK2Gl5Sp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1242561713842643000,
+					"id_str": "1242561713842642944",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1242561713842642944/pu/img/emEQJBdZ9FWY-Nip.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1242561713842642944/pu/img/emEQJBdZ9FWY-Nip.jpg",
+					"url": "https://t.co/m9QK2Gl5Sp",
+					"display_url": "pic.twitter.com/m9QK2Gl5Sp",
+					"expanded_url": "https://twitter.com/sarajcox/status/1242561773921763329/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1242561713842643000,
+					"id_str": "1242561713842642944",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1242561713842642944/pu/img/emEQJBdZ9FWY-Nip.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1242561713842642944/pu/img/emEQJBdZ9FWY-Nip.jpg",
+					"url": "https://t.co/m9QK2Gl5Sp",
+					"display_url": "pic.twitter.com/m9QK2Gl5Sp",
+					"expanded_url": "https://twitter.com/sarajcox/status/1242561773921763329/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 528,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							10,
+							11
+						],
+						"duration_millis": 89378,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1242561713842642944/pu/vid/320x352/Cl-W11VHrl5795g4.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1242561713842642944/pu/pl/gbPaRcuJa1-A5wpZ.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1242561713842642944/pu/vid/360x396/BYJzQWw2paB1drDC.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1242561713842642944/pu/vid/480x528/DTlrkwaZJ5hN57ye.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 33174013,
+			"id_str": "33174013",
+			"name": "sara cox",
+			"screen_name": "sarajcox",
+			"location": "gawping in the fridge",
+			"description": "Join me teatimes BBC Radio2 5-7pm mon-fri 💪🏽BBC2: “Back in time for the Shop” tues 8pm. Book “Till The Cows Come Home”",
+			"url": "https://t.co/8S6oUR5ygW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8S6oUR5ygW",
+							"expanded_url": "http://www.troikatalent.com",
+							"display_url": "troikatalent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 828197,
+			"friends_count": 2295,
+			"listed_count": 2408,
+			"created_at": "Sun Apr 19 10:27:43 +0000 2009",
+			"favourites_count": 5141,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 33100,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1167794855336185859/xVosCFnR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1167794855336185859/xVosCFnR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/33174013/1583435068",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 482,
+		"favorite_count": 2979,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ladies &amp; gentlemen, we have a winner. She just gets better &amp; better. (nb : Bit o’ cussin if there’s a child nearby) https://t.co/m9QK2Gl5Sp"
+	},
+	{
 		"created_at": "Tue Mar 24 00:54:22 +0000 2020",
 		"id": 1242242026357260300,
 		"id_str": "1242253386713632769",
