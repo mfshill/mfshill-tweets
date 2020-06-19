b238472b6a9b02894a2bf56adc37cbc2c26dd419
mfshill <mh-signup@outlook.com>
2020-06-19 02:17:40 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40926,
-	"created_at": "Thu Jun 18 01:17:38 +0000 2020",
+	"tweets": 40928,
+	"created_at": "Fri Jun 19 01:17:38 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,310 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Thu Jun 18 20:03:33 +0000 2020",
+		"id": 1273707946959089700,
+		"id_str": "1273707946959089664",
+		"full_text": "@LollyStrnks Hammer? https://t.co/8Sh0m6kqwX",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LollyStrnks",
+					"name": "Lauren",
+					"id": 132503065,
+					"id_str": "132503065",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1273707939803672600,
+					"id_str": "1273707939803672580",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ea0en3OXsAQy9K8.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ea0en3OXsAQy9K8.jpg",
+					"url": "https://t.co/8Sh0m6kqwX",
+					"display_url": "pic.twitter.com/8Sh0m6kqwX",
+					"expanded_url": "https://twitter.com/mfshill/status/1273707946959089664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 124,
+							"h": 124,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1273707939803672600,
+					"id_str": "1273707939803672580",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ea0en3OXsAQy9K8.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ea0en3OXsAQy9K8.jpg",
+					"url": "https://t.co/8Sh0m6kqwX",
+					"display_url": "pic.twitter.com/8Sh0m6kqwX",
+					"expanded_url": "https://twitter.com/mfshill/status/1273707946959089664/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 124,
+							"h": 124,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 124,
+							"h": 178,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							62,
+							89
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Ea0en3OXsAQy9K8.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1273704201781219300,
+		"in_reply_to_status_id_str": "1273704201781219329",
+		"in_reply_to_user_id": 132503065,
+		"in_reply_to_user_id_str": "132503065",
+		"in_reply_to_screen_name": "LollyStrnks",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": "https://t.co/ZCrqLtp1Br",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZCrqLtp1Br",
+							"expanded_url": "https://cinehabitue.wordpress.com/",
+							"display_url": "cinehabitue.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 266,
+			"friends_count": 274,
+			"listed_count": 23,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1074,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 40604,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "da",
+		"text": "@LollyStrnks Hammer? https://t.co/8Sh0m6kqwX"
+	},
+	{
+		"created_at": "Thu Jun 18 18:10:38 +0000 2020",
+		"id": 1273679529802956800,
+		"id_str": "1273679529802956806",
+		"full_text": "@strydrunning hi guys, any chance you can put back the battery % in the android app? so much easier to use rather than the colour coded icon. I know it briefly shows the % when on low battery but it's not persistent.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "strydrunning",
+					"name": "Stryd | Run With Power",
+					"id": 2842023336,
+					"id_str": "2842023336",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 2842023336,
+		"in_reply_to_user_id_str": "2842023336",
+		"in_reply_to_screen_name": "strydrunning",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": "https://t.co/ZCrqLtp1Br",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZCrqLtp1Br",
+							"expanded_url": "https://cinehabitue.wordpress.com/",
+							"display_url": "cinehabitue.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 266,
+			"friends_count": 274,
+			"listed_count": 23,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1074,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 40604,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@strydrunning hi guys, any chance you can put back the battery % in the android app? so much easier to use rather than the colour coded icon. I know it briefly shows the % when on low battery but it's not persistent."
+	},
+	{
 		"created_at": "Wed Jun 17 21:39:19 +0000 2020",
 		"id": 1273364259263701000,
 		"id_str": "1273369659388108802",
@@ -178,7 +482,7 @@ Grailbird.data.tweets_2020_06 =
 		"created_at": "Wed Jun 17 21:38:20 +0000 2020",
 		"id": 1273369413086052400,
 		"id_str": "1273369413086052352",
-		"full_text": "well done for making zero effort to read the article you've linked to 🤦🏾‍♂️ https://t.co/c8Km6zBWvF",
+		"full_text": "well done for making zero effort to read the article you've linked to 🤦🏾♂️ https://t.co/c8Km6zBWvF",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -478,7 +782,7 @@ Grailbird.data.tweets_2020_06 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "well done for making zero effort to read the article you've linked to 🤦🏾‍♂️ https://t.co/c8Km6zBWvF"
+		"text": "well done for making zero effort to read the article you've linked to 🤦🏾♂️ https://t.co/c8Km6zBWvF"
 	},
 	{
 		"created_at": "Wed Jun 17 21:20:01 +0000 2020",
