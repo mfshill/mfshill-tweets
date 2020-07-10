dd128c14b484bee8cbb1853ae0dd699750ad6695
mfshill <mh-signup@outlook.com>
2020-07-10 02:19:37 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41009,
-	"created_at": "Thu Jul 09 01:17:39 +0000 2020",
+	"tweets": 41011,
+	"created_at": "Fri Jul 10 01:19:36 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,421 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 09 20:47:46 +0000 2020",
+		"id": 1281319524709654500,
+		"id_str": "1281329219574931462",
+		"full_text": "How United win penalties https://t.co/JEf7oImbUZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1281319503670972400,
+					"id_str": "1281319503670972421",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/EcgpTMQWkAUucar.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcgpTMQWkAUucar.jpg",
+					"url": "https://t.co/JEf7oImbUZ",
+					"display_url": "pic.twitter.com/JEf7oImbUZ",
+					"expanded_url": "https://twitter.com/SmolManSyndrome/status/1281319524709654534/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1281319503670972400,
+					"id_str": "1281319503670972421",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/EcgpTMQWkAUucar.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcgpTMQWkAUucar.jpg",
+					"url": "https://t.co/JEf7oImbUZ",
+					"display_url": "pic.twitter.com/JEf7oImbUZ",
+					"expanded_url": "https://twitter.com/SmolManSyndrome/status/1281319524709654534/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281319502978994200,
+					"id_str": "1281319502978994177",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/EcgpTJrX0AEWWkS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcgpTJrX0AEWWkS.jpg",
+					"url": "https://t.co/JEf7oImbUZ",
+					"display_url": "pic.twitter.com/JEf7oImbUZ",
+					"expanded_url": "https://twitter.com/SmolManSyndrome/status/1281319524709654534/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281319503394152400,
+					"id_str": "1281319503394152458",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/EcgpTLOWoAoWlFq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcgpTLOWoAoWlFq.jpg",
+					"url": "https://t.co/JEf7oImbUZ",
+					"display_url": "pic.twitter.com/JEf7oImbUZ",
+					"expanded_url": "https://twitter.com/SmolManSyndrome/status/1281319524709654534/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1691660443,
+			"id_str": "1691660443",
+			"name": "Ron Rockstone",
+			"screen_name": "SmolManSyndrome",
+			"location": "",
+			"description": "You’re asking me about the moral character of a man named Rat Fucker Sam?",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2008,
+			"friends_count": 893,
+			"listed_count": 19,
+			"created_at": "Thu Aug 22 17:21:51 +0000 2013",
+			"favourites_count": 38527,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 128224,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1265706188647403521/3FowYhYn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1265706188647403521/3FowYhYn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1691660443/1586655121",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 221,
+		"favorite_count": 649,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "How United win penalties https://t.co/JEf7oImbUZ"
+	},
+	{
+		"created_at": "Thu Jul 09 16:00:57 +0000 2020",
+		"id": 1281257041298948000,
+		"id_str": "1281257041298948097",
+		"full_text": "Acceptable Risk #xkcd https://t.co/qyV1M63Jg6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "xkcd",
+					"indices": [
+						16,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1281256979298693000,
+					"id_str": "1281256979298693127",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecfwby0WsAcL9C0.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecfwby0WsAcL9C0.png",
+					"url": "https://t.co/qyV1M63Jg6",
+					"display_url": "pic.twitter.com/qyV1M63Jg6",
+					"expanded_url": "https://twitter.com/mfshill/status/1281257041298948097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 740,
+							"h": 255,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 740,
+							"h": 255,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 234,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1281256979298693000,
+					"id_str": "1281256979298693127",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecfwby0WsAcL9C0.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecfwby0WsAcL9C0.png",
+					"url": "https://t.co/qyV1M63Jg6",
+					"display_url": "pic.twitter.com/qyV1M63Jg6",
+					"expanded_url": "https://twitter.com/mfshill/status/1281257041298948097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 740,
+							"h": 255,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 740,
+							"h": 255,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 234,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": "https://t.co/ZCrqLtp1Br",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZCrqLtp1Br",
+							"expanded_url": "https://cinehabitue.wordpress.com/",
+							"display_url": "cinehabitue.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 266,
+			"friends_count": 274,
+			"listed_count": 23,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1086,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 40702,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Acceptable Risk #xkcd https://t.co/qyV1M63Jg6"
+	},
+	{
 		"created_at": "Wed Jul 08 19:10:29 +0000 2020",
 		"id": 1280942349988892700,
 		"id_str": "1280942349988892673",
