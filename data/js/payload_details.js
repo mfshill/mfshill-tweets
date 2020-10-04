80994b5f3f7031593dcdd0f187720cd53cfc1dc3
mfshill <mh-signup@outlook.com>
2020-10-04 02:17:40 +0100

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41295,
-	"created_at": "Sat Oct 03 01:17:37 +0000 2020",
+	"tweets": 41299,
+	"created_at": "Sun Oct 04 01:17:39 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,570 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sat Oct 03 19:11:42 +0000 2020",
+		"id": 1312470398328475600,
+		"id_str": "1312470398328475648",
+		"full_text": "@_sinesapiens_ @Jazz_Pirate On Netflix if you're interested... https://t.co/rMppt0lFYT",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_sinesapiens_",
+					"name": "𝔖𝔦𝔫𝔢𝔰𝔞𝔭𝔦𝔢𝔫𝔰",
+					"id": 1116763141990494200,
+					"id_str": "1116763141990494208",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "Jazz_Pirate",
+					"name": "Steven isolatingly Challenged🎮",
+					"id": 153810457,
+					"id_str": "153810457",
+					"indices": [
+						15,
+						27
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rMppt0lFYT",
+					"expanded_url": "https://www.netflix.com/title/699938",
+					"display_url": "netflix.com/title/699938",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1312422927355326500,
+		"in_reply_to_status_id_str": "1312422927355326464",
+		"in_reply_to_user_id": 1116763141990494200,
+		"in_reply_to_user_id_str": "1116763141990494208",
+		"in_reply_to_screen_name": "_sinesapiens_",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 267,
+			"friends_count": 258,
+			"listed_count": 23,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41037,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182378753781125121/7rMCpq6M_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@_sinesapiens_ @Jazz_Pirate On Netflix if you're interested... https://t.co/rMppt0lFYT"
+	},
+	{
+		"created_at": "Sat Oct 03 18:57:57 +0000 2020",
+		"id": 1312453259152380000,
+		"id_str": "1312466939516612610",
+		"full_text": "There are a lot of reasons for thinking Trump will pull through, such as the likelihood that Hell will reject his soul",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17336372,
+			"id_str": "17336372",
+			"name": "Frankie Boyle",
+			"screen_name": "frankieboyle",
+			"location": "www.frankieboyle.com",
+			"description": "Some laugh",
+			"url": "https://t.co/ANwcwPmQpN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ANwcwPmQpN",
+							"expanded_url": "http://www.instagram.com/frankie_boyle",
+							"display_url": "instagram.com/frankie_boyle",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2817667,
+			"friends_count": 1142,
+			"listed_count": 5402,
+			"created_at": "Wed Nov 12 09:48:13 +0000 2008",
+			"favourites_count": 6832,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 21346,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1307385728419213312/JmFrlQ5C_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1307385728419213312/JmFrlQ5C_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17336372/1600540197",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1216,
+		"favorite_count": 10286,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "There are a lot of reasons for thinking Trump will pull through, such as the likelihood that Hell will reject his soul"
+	},
+	{
+		"created_at": "Sat Oct 03 18:52:29 +0000 2020",
+		"id": 1312392447343964200,
+		"id_str": "1312465562128142336",
+		"full_text": "The Partey is over everyone left an Aouar ago https://t.co/EK5j54ItJr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EK5j54ItJr",
+					"expanded_url": "https://twitter.com/EduardoHagn/status/1311803730275303426",
+					"display_url": "twitter.com/EduardoHagn/st…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2559885495,
+			"id_str": "2559885495",
+			"name": "Ché",
+			"screen_name": "UtdChe",
+			"location": "Cape Town, South Africa",
+			"description": "All I have are negative thoughts || Fan Account #GlazersOut #WoodwardOut @ManUtd @HoustonRockets @thenbhd",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14646,
+			"friends_count": 995,
+			"listed_count": 76,
+			"created_at": "Fri May 23 06:52:15 +0000 2014",
+			"favourites_count": 249139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 46556,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1307251246861090818/hqO9N1ZQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1307251246861090818/hqO9N1ZQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2559885495/1601752804",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1311803730275303400,
+		"quoted_status_id_str": "1311803730275303426",
+		"quoted_status_permalink": {
+			"url": "https://t.co/EK5j54ItJr",
+			"expanded": "https://twitter.com/EduardoHagn/status/1311803730275303426",
+			"display": "twitter.com/EduardoHagn/st…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 01 23:02:36 +0000 2020",
+			"id": 1311803730275303400,
+			"id_str": "1311803730275303426",
+			"full_text": "Can’t wait to play Manchester United with Partey &amp; Aouar.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				61
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3120207231,
+				"id_str": "3120207231",
+				"name": "Eduardo Hagn",
+				"screen_name": "EduardoHagn",
+				"location": "Miami, FL",
+				"description": "Future Journalist. Blessed to be Chilean 🇨🇱! Instagram: @eduardo_hagn7. @Arsenal.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 51782,
+				"friends_count": 865,
+				"listed_count": 248,
+				"created_at": "Fri Mar 27 00:54:25 +0000 2015",
+				"favourites_count": 34434,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 36925,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1300167595019120646/56Jhix3o_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1300167595019120646/56Jhix3o_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3120207231/1597759422",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 172,
+			"favorite_count": 4354,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 587,
+		"favorite_count": 4373,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "The Partey is over everyone left an Aouar ago https://t.co/EK5j54ItJr"
+	},
+	{
+		"created_at": "Sat Oct 03 11:36:25 +0000 2020",
+		"id": 1312116863619362800,
+		"id_str": "1312355822316457984",
+		"full_text": "This is a free website https://t.co/OHT2EKqyIb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1312116858913423400,
+					"id_str": "1312116858913423361",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/EjWTVM9WAAEMo6M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjWTVM9WAAEMo6M.jpg",
+					"url": "https://t.co/OHT2EKqyIb",
+					"display_url": "pic.twitter.com/OHT2EKqyIb",
+					"expanded_url": "https://twitter.com/Carrasquillo/status/1312116863619362816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 975,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1241,
+							"h": 1528,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1312116858913423400,
+					"id_str": "1312116858913423361",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/EjWTVM9WAAEMo6M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjWTVM9WAAEMo6M.jpg",
+					"url": "https://t.co/OHT2EKqyIb",
+					"display_url": "pic.twitter.com/OHT2EKqyIb",
+					"expanded_url": "https://twitter.com/Carrasquillo/status/1312116863619362816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 975,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 552,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1241,
+							"h": 1528,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21688902,
+			"id_str": "21688902",
+			"name": "Adrian Carrasquillo",
+			"screen_name": "Carrasquillo",
+			"location": "New York, NY",
+			"description": "National political reporter @Newsweek covering Biden | Reporting the Latino political story | @sanbenito scholar |Send me scoops 👇🏽a.carrasquillo@newsweek.com",
+			"url": "https://t.co/GZwh8SYOLw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GZwh8SYOLw",
+							"expanded_url": "https://www.newsweek.com/authors/adrian-carrasquillo",
+							"display_url": "newsweek.com/authors/adrian…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55370,
+			"friends_count": 6872,
+			"listed_count": 1597,
+			"created_at": "Mon Feb 23 20:20:36 +0000 2009",
+			"favourites_count": 34633,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 68641,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1237977324043276289/3Q8uI0jF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1237977324043276289/3Q8uI0jF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21688902/1600701135",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "5CD6FF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 897,
+		"favorite_count": 4336,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a free website https://t.co/OHT2EKqyIb"
+	},
+	{
 		"created_at": "Fri Oct 02 13:09:23 +0000 2020",
 		"id": 1312016830739943400,
 		"id_str": "1312016830739943424",
