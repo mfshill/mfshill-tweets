da75c3a9be726ef5c864bc2b1129a9fe32847ca8
mfshill <mh-signup@outlook.com>
2020-10-22 02:17:40 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41356,
-	"created_at": "Wed Oct 21 01:17:39 +0000 2020",
+	"tweets": 41357,
+	"created_at": "Thu Oct 22 01:17:38 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 22 00:12:44 +0000 2020",
+		"id": 1319069137856061400,
+		"id_str": "1319069137856061440",
+		"full_text": "Why Running Won’t Ruin Your Knees - The New York Times - https://t.co/U24p0YWvio",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U24p0YWvio",
+					"expanded_url": "https://www.nytimes.com/2020/10/21/well/why-running-wont-ruin-your-knees.html",
+					"display_url": "nytimes.com/2020/10/21/wel…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://klinkerapps.com\" rel=\"nofollow\">Talon Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 265,
+			"friends_count": 258,
+			"listed_count": 23,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1143,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41104,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1317792662104297473/Coj3BUfH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1317792662104297473/Coj3BUfH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Why Running Won’t Ruin Your Knees - The New York Times - https://t.co/U24p0YWvio"
+	},
+	{
 		"created_at": "Tue Oct 20 13:04:17 +0000 2020",
 		"id": 1318523884308660200,
 		"id_str": "1318538530252009474",
@@ -120,7 +213,7 @@ Grailbird.data.tweets_2020_10 =
 			"name": "DUKER",
 			"screen_name": "sophiedukebox",
 			"location": "Tokyo",
-			"description": "slutty clown 🤡 she/her/that bitch 🧞‍♀️I too am enraged I’m doing so well 🔥",
+			"description": "slutty clown 🤡 she/her/that bitch 🧞♀️I too am enraged I’m doing so well 🔥",
 			"url": "https://t.co/LWBfILc5on",
 			"entities": {
 				"url": {
