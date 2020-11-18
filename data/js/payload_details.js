3c8e6e728db3c4f0ed3b5e0d7bb8a3ca599e0f7b
mfshill <mh-signup@outlook.com>
2020-11-18 01:17:39 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41446,
-	"created_at": "Mon Nov 16 01:17:38 +0000 2020",
+	"tweets": 41447,
+	"created_at": "Wed Nov 18 01:17:38 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 00:51:51 +0000 2020",
+		"id": 1328840324429516800,
+		"id_str": "1328863454707511296",
+		"full_text": "Well thanks, Newton, clearly I didn't put my lockdown to good use! https://t.co/ta6X2vAJ1n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1328840321057382400,
+					"id_str": "1328840321057382400",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EnD9O0PXYAAO5rQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnD9O0PXYAAO5rQ.jpg",
+					"url": "https://t.co/ta6X2vAJ1n",
+					"display_url": "pic.twitter.com/ta6X2vAJ1n",
+					"expanded_url": "https://twitter.com/JoshuaGladwin/status/1328840324429516800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1328840321057382400,
+					"id_str": "1328840321057382400",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EnD9O0PXYAAO5rQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnD9O0PXYAAO5rQ.jpg",
+					"url": "https://t.co/ta6X2vAJ1n",
+					"display_url": "pic.twitter.com/ta6X2vAJ1n",
+					"expanded_url": "https://twitter.com/JoshuaGladwin/status/1328840324429516800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1101946809725972500,
+			"id_str": "1101946809725972480",
+			"name": "Joshua Gladwin",
+			"screen_name": "JoshuaGladwin",
+			"location": "Berlin/ベルリン/柏林/베를린/ברלין",
+			"description": "Newbie Data Scientist. MSc AI @UniofBath.\nLover of Linguistics & Science. All-Round Geek.\nOften with @ChristopherWard & @SuluTheCat\n🏳️‍🌈  https://t.co/h4IxIYLYdk",
+			"url": "https://t.co/CATTyrQl33",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CATTyrQl33",
+							"expanded_url": "https://joshuagladwin.de/",
+							"display_url": "joshuagladwin.de",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/h4IxIYLYdk",
+							"expanded_url": "http://pronoun.is/he",
+							"display_url": "pronoun.is/he",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 878,
+			"friends_count": 881,
+			"listed_count": 1,
+			"created_at": "Sat Mar 02 20:46:07 +0000 2019",
+			"favourites_count": 8582,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3475,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152664015518277639/B8feVnfT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152664015518277639/B8feVnfT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1101946809725972480/1603648885",
+			"profile_link_color": "FF69B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Well thanks, Newton, clearly I didn't put my lockdown to good use! https://t.co/ta6X2vAJ1n"
+	},
+	{
 		"created_at": "Sun Nov 15 11:57:51 +0000 2020",
 		"id": 1327943896312189000,
 		"id_str": "1327943896312188928",
