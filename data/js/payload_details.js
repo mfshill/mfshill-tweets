c1972b976a89a00d7eef8a807e6b575d21642734
mfshill <mh-signup@outlook.com>
2020-11-20 01:17:40 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41448,
-	"created_at": "Thu Nov 19 01:17:38 +0000 2020",
+	"tweets": 41449,
+	"created_at": "Fri Nov 20 01:17:38 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 19 19:33:25 +0000 2020",
+		"id": 1329406445222301700,
+		"id_str": "1329508096641732608",
+		"full_text": "Imagine this\n\nYou wake up in bed with your partner, your little ones are asleep in their bedrooms. It’s 3am. You hear a noise downstairs. It sounds like a scraping and a scuffling. \n\nYou realise it’s someone either trying to get into your house, or someone is in your house.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 412627022,
+			"id_str": "412627022",
+			"name": "CrimeGirl",
+			"screen_name": "CrimeGirI",
+			"location": "Closer than you think",
+			"description": "Barrister • Army wife • Crime • Military • a fo ben, bid bont •",
+			"url": "https://t.co/xMmesHOJyE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xMmesHOJyE",
+							"expanded_url": "http://www.crimegirl.co.uk",
+							"display_url": "crimegirl.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32176,
+			"friends_count": 1000,
+			"listed_count": 129,
+			"created_at": "Mon Nov 14 23:35:34 +0000 2011",
+			"favourites_count": 53479,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 13596,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1190744817468350471/-P4Mb-Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1190744817468350471/-P4Mb-Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/412627022/1515934507",
+			"profile_link_color": "121942",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1409,
+		"favorite_count": 4661,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Imagine this\n\nYou wake up in bed with your partner, your little ones are asleep in their bedrooms. It’s 3am. You hear a noise downstairs. It sounds like a scraping and a scuffling. \n\nYou realise it’s someone either trying to get into your house, or someone is in your house."
+	},
+	{
 		"created_at": "Thu Nov 19 00:17:13 +0000 2020",
 		"id": 1329217128185159700,
 		"id_str": "1329217128185159681",
