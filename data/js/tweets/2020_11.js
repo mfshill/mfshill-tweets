718445352f04b3a08a9ecf33d46a3fa4db219011
mfshill <mh-signup@outlook.com>
2020-12-01 01:17:40 +0000

    Added 1 tweets

--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 30 22:44:20 +0000 2020",
+		"id": 1333515475028885500,
+		"id_str": "1333542406134829057",
+		"full_text": "“How’s lockdown going?”https://t.co/9ybT98KBGr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1333386967036399600,
+					"id_str": "1333386967036399617",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1333386967036399617/pu/img/Ps11pbzvZg3sLWMB.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1333386967036399617/pu/img/Ps11pbzvZg3sLWMB.jpg",
+					"url": "https://t.co/9ybT98KBGr",
+					"display_url": "pic.twitter.com/9ybT98KBGr",
+					"expanded_url": "https://twitter.com/damienrobi/status/1333387129041408002/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1333387129041408000,
+					"source_status_id_str": "1333387129041408002",
+					"source_user_id": 173809097,
+					"source_user_id_str": "173809097"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1333386967036399600,
+					"id_str": "1333386967036399617",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1333386967036399617/pu/img/Ps11pbzvZg3sLWMB.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1333386967036399617/pu/img/Ps11pbzvZg3sLWMB.jpg",
+					"url": "https://t.co/9ybT98KBGr",
+					"display_url": "pic.twitter.com/9ybT98KBGr",
+					"expanded_url": "https://twitter.com/damienrobi/status/1333387129041408002/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1333387129041408000,
+					"source_status_id_str": "1333387129041408002",
+					"source_user_id": 173809097,
+					"source_user_id_str": "173809097",
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 102217,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1333386967036399617/pu/vid/720x1280/vvSwKMeRsSBVRMkU.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1333386967036399617/pu/pl/Et8RXHtg-oUNz1_O.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1333386967036399617/pu/vid/360x640/TlybpZ3Tv1TcZRE1.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1333386967036399617/pu/vid/320x568/1ZWLuESJy2FMDgjc.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 173809097,
+							"id_str": "173809097",
+							"name": "Damien Robitaille",
+							"screen_name": "damienrobi",
+							"location": "Montreal",
+							"description": "Je chante, I sing, Canto",
+							"url": "https://t.co/MVnaS8vTjU",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/MVnaS8vTjU",
+											"expanded_url": "http://www.damienrobitaille.com",
+											"display_url": "damienrobitaille.com",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 17056,
+							"friends_count": 1343,
+							"listed_count": 123,
+							"created_at": "Mon Aug 02 12:13:00 +0000 2010",
+							"favourites_count": 4339,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": false,
+							"statuses_count": 3714,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "C0DEED",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1246415456468119557/r-X2TEqV_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1246415456468119557/r-X2TEqV_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/173809097/1586003636",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 994502112469049300,
+			"id_str": "994502112469049344",
+			"name": "mnrrntt2 & 85 others",
+			"screen_name": "mnrrntt",
+			"location": "",
+			"description": "Big fan of terrible photoshop and straight to DVD crap movies",
+			"url": "https://t.co/Xncgdjv171",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Xncgdjv171",
+							"expanded_url": "http://www.dignitas.ch/?lang=en",
+							"display_url": "dignitas.ch/?lang=en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3978,
+			"friends_count": 828,
+			"listed_count": 9,
+			"created_at": "Thu May 10 08:59:15 +0000 2018",
+			"favourites_count": 120436,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 44169,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1323203217803661313/sH_k_ELZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1323203217803661313/sH_k_ELZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/994502112469049344/1605297969",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 151,
+		"favorite_count": 640,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“How’s lockdown going?”https://t.co/9ybT98KBGr"
+	},
+	{
 		"created_at": "Sun Nov 29 21:16:27 +0000 2020",
 		"id": 1333157902622859300,
 		"id_str": "1333157902622859270",
