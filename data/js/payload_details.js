8856d8b5e86ad39557b26ab178f067fb16c496b1
mfshill <mh-signup@outlook.com>
2020-12-31 01:17:40 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41593,
-	"created_at": "Wed Dec 30 01:17:38 +0000 2020",
+	"tweets": 41594,
+	"created_at": "Thu Dec 31 01:17:39 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 31 00:00:14 +0000 2020",
+		"id": 1344433144267755500,
+		"id_str": "1344433144267755525",
+		"full_text": "@ryanarey it was far too long (dragging in many places), the 80s aesthetic didn't really work, the villain was terrible, they could &amp; should have developed Wiig's character more (the one good element of the movie), it was ONE HUNDRED &amp; FIFTY MINUTES LONG! The first film was so much better.",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			298
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ryanarey",
+					"name": "Ryan Arey",
+					"id": 82921614,
+					"id_str": "82921614",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1344429469319245800,
+		"in_reply_to_status_id_str": "1344429469319245836",
+		"in_reply_to_user_id": 82921614,
+		"in_reply_to_user_id_str": "82921614",
+		"in_reply_to_screen_name": "ryanarey",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\"\n(Strength, balance, courage & common sense)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 264,
+			"friends_count": 261,
+			"listed_count": 22,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1172,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 41369,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1343985344266592262/egU9-x71_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1343985344266592262/egU9-x71_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ryanarey it was far too long (dragging in many places), the 80s aesthetic didn't really work, the villain was terrible, they could &amp; should have developed Wiig's character more (the one good element of the movie), it was ONE HUNDRED &amp; FIFTY MINUTES LONG! The first film was so much better."
+	},
+	{
 		"created_at": "Tue Dec 29 19:22:33 +0000 2020",
 		"id": 1344000875061244000,
 		"id_str": "1344000875061243904",
