d4ba4ee5ccf8fae02d4c0a83784899269819c11e
mfshill <mh-signup@outlook.com>
2021-01-02 01:17:39 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41599,
-	"created_at": "Fri Jan 01 01:17:38 +0000 2021",
+	"tweets": 41600,
+	"created_at": "Sat Jan 02 01:17:38 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_01.js
@@ -0,0 +1,210 @@
+Grailbird.data.tweets_2021_01 = 
+[
+	{
+		"created_at": "Fri Jan 01 23:18:43 +0000 2021",
+		"id": 1345135020634202000,
+		"id_str": "1345147469592727552",
+		"full_text": "Probably my favourite tweet of 2020 https://t.co/8G2fURJPEk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8G2fURJPEk",
+					"expanded_url": "https://twitter.com/oilysailor/status/1292930082135248896",
+					"display_url": "twitter.com/oilysailor/sta…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 60026651,
+			"id_str": "60026651",
+			"name": "Orbinho",
+			"screen_name": "Orbinho",
+			"location": "London-ish",
+			"description": "Arsenal facts. Head of Opta Data Editorial since the end of the 20th Century. Creator of @OptaJoe Trying to convince people to use data better.",
+			"url": "https://t.co/cRoW8ZO4IK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cRoW8ZO4IK",
+							"expanded_url": "https://www.statsperform.com/",
+							"display_url": "statsperform.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71408,
+			"friends_count": 578,
+			"listed_count": 1654,
+			"created_at": "Sat Jul 25 09:55:31 +0000 2009",
+			"favourites_count": 585,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 27846,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "642D8B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/588984322130083840/bc56JV5r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/588984322130083840/bc56JV5r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/60026651/1356111874",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "65B0DA",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1292930082135249000,
+		"quoted_status_id_str": "1292930082135248896",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8G2fURJPEk",
+			"expanded": "https://twitter.com/oilysailor/status/1292930082135248896",
+			"display": "twitter.com/oilysailor/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Aug 10 21:05:27 +0000 2020",
+			"id": 1292930082135249000,
+			"id_str": "1292930082135248896",
+			"full_text": "\"Man Utd have won a penalty\" now autocompletes when I type \"M\"",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				62
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17896113,
+				"id_str": "17896113",
+				"name": "Duncan Alexander",
+				"screen_name": "oilysailor",
+				"location": "August 15 1992, 3pm",
+				"description": "Luxury sports content | Author of Outside The Box & The @OptaJoe Football Yearbook",
+				"url": "https://t.co/17N8dvO3CL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/17N8dvO3CL",
+								"expanded_url": "https://www.twitch.tv/oilysailor",
+								"display_url": "twitch.tv/oilysailor",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 78760,
+				"friends_count": 996,
+				"listed_count": 1034,
+				"created_at": "Fri Dec 05 12:29:22 +0000 2008",
+				"favourites_count": 5741,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 39676,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1026565091817345024/n9e8ynd__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1026565091817345024/n9e8ynd__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17896113/1594149927",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "AD0505",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2211,
+			"favorite_count": 19282,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 22,
+		"favorite_count": 125,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Probably my favourite tweet of 2020 https://t.co/8G2fURJPEk"
+	}
+]
