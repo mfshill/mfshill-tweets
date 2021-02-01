0e477ef319759acab96698dbfbadfb06f132012f
mfshill <mh-signup@outlook.com>
2021-02-01 01:17:40 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41723,
-	"created_at": "Sun Jan 31 01:17:39 +0000 2021",
+	"tweets": 41724,
+	"created_at": "Mon Feb 01 01:17:39 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 31 21:14:59 +0000 2021",
+		"id": 1355984368628625400,
+		"id_str": "1355987967098171393",
+		"full_text": "Tottenham making pre-Christmas Arsenal look like the Invincibles.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20481323,
+			"id_str": "20481323",
+			"name": "Mark Mann-Bryans",
+			"screen_name": "MarkyMBryans",
+			"location": "London",
+			"description": "Sports Journalist for @pasport - covering Arsenal and England. Once saw Mohamed Elneny captain #AFC",
+			"url": "https://t.co/LZG0Y2oOiB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LZG0Y2oOiB",
+							"expanded_url": "https://www.pressassociation.com/sport/",
+							"display_url": "pressassociation.com/sport/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16778,
+			"friends_count": 1091,
+			"listed_count": 324,
+			"created_at": "Tue Feb 10 00:38:07 +0000 2009",
+			"favourites_count": 1794,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 18360,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1337142583361933320/rAgYKv8Z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1337142583361933320/rAgYKv8Z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20481323/1601381777",
+			"profile_link_color": "ED2F4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "C3E353",
+			"profile_text_color": "FE243C",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 275,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Tottenham making pre-Christmas Arsenal look like the Invincibles."
+	},
+	{
 		"created_at": "Sat Jan 30 19:26:20 +0000 2021",
 		"id": 1355598236384440300,
 		"id_str": "1355598236384440326",
