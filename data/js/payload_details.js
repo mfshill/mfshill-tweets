1955299df286b24d068476a93ac448db704abfb3
mfshill <mh-signup@outlook.com>
2021-02-18 01:17:38 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41775,
-	"created_at": "Wed Feb 17 01:17:37 +0000 2021",
+	"tweets": 41776,
+	"created_at": "Thu Feb 18 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Wed Feb 17 10:10:08 +0000 2021",
+		"id": 1361791030757007400,
+		"id_str": "1361981246356742145",
+		"full_text": "Happy loves you all! Thanks for everything! https://t.co/p1aY9JcFWh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1361790849529487400,
+					"id_str": "1361790849529487361",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1361790849529487361/pu/img/iX17VBDJleIrLBiC.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1361790849529487361/pu/img/iX17VBDJleIrLBiC.jpg",
+					"url": "https://t.co/p1aY9JcFWh",
+					"display_url": "pic.twitter.com/p1aY9JcFWh",
+					"expanded_url": "https://twitter.com/AdamSandler/status/1361791030757007360/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1361790849529487400,
+					"id_str": "1361790849529487361",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1361790849529487361/pu/img/iX17VBDJleIrLBiC.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1361790849529487361/pu/img/iX17VBDJleIrLBiC.jpg",
+					"url": "https://t.co/p1aY9JcFWh",
+					"display_url": "pic.twitter.com/p1aY9JcFWh",
+					"expanded_url": "https://twitter.com/AdamSandler/status/1361791030757007360/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 29733,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1361790849529487361/pu/vid/1280x720/pp1D068xTKASi2Vn.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1361790849529487361/pu/vid/480x270/P2-sbclt6eXOn816.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1361790849529487361/pu/pl/g_KalX5rp7dXq705.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1361790849529487361/pu/vid/640x360/tzuI1GIPEA_ZWcQq.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 316756361,
+			"id_str": "316756361",
+			"name": "Adam Sandler",
+			"screen_name": "AdamSandler",
+			"location": "",
+			"description": "HUBIE HALLOWEEN now screaming on Netflix 🎃",
+			"url": "https://t.co/UV3iXlS7Su",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UV3iXlS7Su",
+							"expanded_url": "https://wbr.ec/100fresh",
+							"display_url": "wbr.ec/100fresh",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2837561,
+			"friends_count": 70,
+			"listed_count": 3923,
+			"created_at": "Mon Jun 13 22:43:10 +0000 2011",
+			"favourites_count": 881,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 756,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1304058356710232064/wsgzlIth_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1304058356710232064/wsgzlIth_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/316756361/1602202142",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 121503,
+		"favorite_count": 721599,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Happy loves you all! Thanks for everything! https://t.co/p1aY9JcFWh"
+	},
+	{
 		"created_at": "Tue Feb 16 10:47:12 +0000 2021",
 		"id": 1361086554588995600,
 		"id_str": "1361628188909199360",
