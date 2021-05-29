bf0ea9f4c9c4ac65cc361fe04ccbb2967e1eea34
mfshill <mh-signup@outlook.com>
2021-05-29 02:17:39 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42016,
-	"created_at": "Thu May 27 01:17:37 +0000 2021",
+	"tweets": 42017,
+	"created_at": "Sat May 29 01:17:38 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 28 21:33:05 +0000 2021",
+		"id": 1398378268680667100,
+		"id_str": "1398391905805910027",
+		"full_text": "https://t.co/FRIFMmQoag",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1398378256781435000,
+					"id_str": "1398378256781434883",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E2gJnZkWYAMpiHO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2gJnZkWYAMpiHO.jpg",
+					"url": "https://t.co/FRIFMmQoag",
+					"display_url": "pic.twitter.com/FRIFMmQoag",
+					"expanded_url": "https://twitter.com/Coldwar_Steve/status/1398378268680667138/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1716,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1005,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1398378256781435000,
+					"id_str": "1398378256781434883",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E2gJnZkWYAMpiHO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2gJnZkWYAMpiHO.jpg",
+					"url": "https://t.co/FRIFMmQoag",
+					"display_url": "pic.twitter.com/FRIFMmQoag",
+					"expanded_url": "https://twitter.com/Coldwar_Steve/status/1398378268680667138/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1716,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1005,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 705709976501817300,
+			"id_str": "705709976501817348",
+			"name": "Cold War Steve",
+			"screen_name": "Coldwar_Steve",
+			"location": "",
+			"description": "Enquiries please email: coldwarsteveoffice@gmail.com For the latest news, subscribe at: https://t.co/EhITpCzpBX Shop at website below",
+			"url": "https://t.co/MOVt5lRPA1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MOVt5lRPA1",
+							"expanded_url": "http://coldwarsteve.com",
+							"display_url": "coldwarsteve.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/EhITpCzpBX",
+							"expanded_url": "https://mailchi.mp/e36989db765c/coldwarsteve",
+							"display_url": "mailchi.mp/e36989db765c/c…",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 327546,
+			"friends_count": 27080,
+			"listed_count": 640,
+			"created_at": "Fri Mar 04 11:02:24 +0000 2016",
+			"favourites_count": 126473,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 38040,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/946296112364875776/PnyQQO7K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/946296112364875776/PnyQQO7K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/705709976501817348/1622189347",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 758,
+		"favorite_count": 3220,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/FRIFMmQoag"
+	},
+	{
 		"created_at": "Wed May 26 22:03:02 +0000 2021",
 		"id": 1397674207648047000,
 		"id_str": "1397674664923697156",
