24c22dea732e76fb58524f12f006bd996fb70468
mfshill <mh-signup@outlook.com>
2021-05-31 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42018,
-	"created_at": "Sun May 30 01:17:37 +0000 2021",
+	"tweets": 42019,
+	"created_at": "Mon May 31 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sun May 30 21:29:08 +0000 2021",
+		"id": 1399113796967358500,
+		"id_str": "1399115687763890178",
+		"full_text": "I see that Brian Cox is trending...\n#TheFastShow https://t.co/fxWFam8NrD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TheFastShow",
+					"indices": [
+						36,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1399113696555802600,
+					"id_str": "1399113696555802626",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1399113696555802626/pu/img/pZernCFa02SmoEjl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1399113696555802626/pu/img/pZernCFa02SmoEjl.jpg",
+					"url": "https://t.co/fxWFam8NrD",
+					"display_url": "pic.twitter.com/fxWFam8NrD",
+					"expanded_url": "https://twitter.com/TheFastShow1/status/1399113796967358466/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 408,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1399113696555802600,
+					"id_str": "1399113696555802626",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1399113696555802626/pu/img/pZernCFa02SmoEjl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1399113696555802626/pu/img/pZernCFa02SmoEjl.jpg",
+					"url": "https://t.co/fxWFam8NrD",
+					"display_url": "pic.twitter.com/fxWFam8NrD",
+					"expanded_url": "https://twitter.com/TheFastShow1/status/1399113796967358466/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 408,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 385,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							30,
+							17
+						],
+						"duration_millis": 20050,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1399113696555802626/pu/vid/476x270/eqJr1gSgzem1US4s.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1399113696555802626/pu/vid/720x408/Ptc_68v224BOMgzx.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1399113696555802626/pu/pl/6ipDm6fo3wsxj93X.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 952298775049130000,
+			"id_str": "952298775049129985",
+			"name": "TheFastShow",
+			"screen_name": "TheFastShow1",
+			"location": "",
+			"description": "Fan account. showing clips from #TheFastShow. content not owned & will remove on request, where appropriate.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 92431,
+			"friends_count": 7834,
+			"listed_count": 125,
+			"created_at": "Sat Jan 13 21:58:15 +0000 2018",
+			"favourites_count": 30152,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14546,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210495613667856385/V9gnEMcA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210495613667856385/V9gnEMcA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/952298775049129985/1605167583",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 66,
+		"favorite_count": 418,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I see that Brian Cox is trending...\n#TheFastShow https://t.co/fxWFam8NrD"
+	},
+	{
 		"created_at": "Sat May 29 12:45:40 +0000 2021",
 		"id": 1398613666476474400,
 		"id_str": "1398621564317405191",
