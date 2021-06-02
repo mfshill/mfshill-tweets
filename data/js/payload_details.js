f40b199c300ba232212b7a70effc82f042b818e8
mfshill <mh-signup@outlook.com>
2021-06-02 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42020,
-	"created_at": "Tue Jun 01 01:17:37 +0000 2021",
+	"tweets": 42021,
+	"created_at": "Wed Jun 02 01:17:37 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,99 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Tue Jun 01 21:25:27 +0000 2021",
+		"id": 1399809229335273500,
+		"id_str": "1399839532837638144",
+		"full_text": "I'm a professional footballer and I've loved not having fans in the ground over the last year. No abuse or criticism for making a mistake, just get on with the game and go home. I'm gutted fans are coming back, so many of them are utter wankers.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://b3ta.com/\" rel=\"nofollow\">fesshole</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1007749631818821600,
+			"id_str": "1007749631818821638",
+			"name": "Fesshole 🙏 ⚫️",
+			"screen_name": "fesshole",
+			"location": "Vatican",
+			"description": "Confess your sins anonymously - will the internet absolve you? 👖 Sponsored by @hebtroco 🩳",
+			"url": "https://t.co/EjDcNEitFa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EjDcNEitFa",
+							"expanded_url": "http://bit.ly/fessholeform",
+							"display_url": "bit.ly/fessholeform",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 139458,
+			"friends_count": 399,
+			"listed_count": 252,
+			"created_at": "Fri Jun 15 22:20:10 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11408,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1172580448662372353/SwJNqDQl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1172580448662372353/SwJNqDQl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1007749631818821638/1562702757",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 167,
+		"favorite_count": 2612,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I'm a professional footballer and I've loved not having fans in the ground over the last year. No abuse or criticism for making a mistake, just get on with the game and go home. I'm gutted fans are coming back, so many of them are utter wankers."
+	}
+]
