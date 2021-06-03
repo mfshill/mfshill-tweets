d93ef703ccfbf685a62c3da2b2dec257214fa399
mfshill <mh-signup@outlook.com>
2021-06-03 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42021,
-	"created_at": "Wed Jun 02 01:17:37 +0000 2021",
+	"tweets": 42022,
+	"created_at": "Thu Jun 03 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,309 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 02 19:18:55 +0000 2021",
+		"id": 1399860017512300500,
+		"id_str": "1400170080307232779",
+		"full_text": "https://t.co/CJcdqq9SGE https://t.co/LXhqMJLCZz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CJcdqq9SGE",
+					"expanded_url": "https://twitter.com/EveningStandard/status/1399679851003527174",
+					"display_url": "twitter.com/EveningStandar…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1399860015536775200,
+					"id_str": "1399860015536775169",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/E21NRHFXoAEUdsN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E21NRHFXoAEUdsN.jpg",
+					"url": "https://t.co/LXhqMJLCZz",
+					"display_url": "pic.twitter.com/LXhqMJLCZz",
+					"expanded_url": "https://twitter.com/Voodoo_Pork/status/1399860017512300545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 371,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 873,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1399860015536775200,
+					"id_str": "1399860015536775169",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/E21NRHFXoAEUdsN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E21NRHFXoAEUdsN.jpg",
+					"url": "https://t.co/LXhqMJLCZz",
+					"display_url": "pic.twitter.com/LXhqMJLCZz",
+					"expanded_url": "https://twitter.com/Voodoo_Pork/status/1399860017512300545/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 371,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 873,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 308901507,
+			"id_str": "308901507",
+			"name": "Voodoo Pork",
+			"screen_name": "Voodoo_Pork",
+			"location": "Nowhere, USA",
+			"description": "Writer || Terminally Online Marxist || Bylines in @ZeroBalanceBlog @StreetFightWCRS @TheDCSentinel",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3700,
+			"friends_count": 995,
+			"listed_count": 35,
+			"created_at": "Wed Jun 01 05:47:14 +0000 2011",
+			"favourites_count": 27106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 56290,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1349579624989257729/eBzxzzpz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1349579624989257729/eBzxzzpz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/308901507/1610599832",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1399679851003527200,
+		"quoted_status_id_str": "1399679851003527174",
+		"quoted_status_permalink": {
+			"url": "https://t.co/CJcdqq9SGE",
+			"expanded": "https://twitter.com/EveningStandard/status/1399679851003527174",
+			"display": "twitter.com/EveningStandar…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 01 10:50:55 +0000 2021",
+			"id": 1399679851003527200,
+			"id_str": "1399679851003527174",
+			"full_text": "Who would you like to see drive around with James Corden? 👨‍🎤🚗 #CarpoolKaraoke https://t.co/aG5iN65ZOW",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				102
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "CarpoolKaraoke",
+						"indices": [
+							63,
+							78
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/aG5iN65ZOW",
+						"expanded_url": "https://bit.ly/3yTaYgn",
+						"display_url": "bit.ly/3yTaYgn",
+						"indices": [
+							79,
+							102
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1168472395,
+				"id_str": "1168472395",
+				"name": "Evening Standard",
+				"screen_name": "EveningStandard",
+				"location": "London, United Kingdom",
+				"description": "Official account of the London Evening Standard 📰\n\nFollow our news account here 👉👉 https://t.co/2T3at4jNvY",
+				"url": "http://t.co/q2y9dLID",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/q2y9dLID",
+								"expanded_url": "http://www.standard.co.uk",
+								"display_url": "standard.co.uk",
+								"indices": [
+									0,
+									20
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/2T3at4jNvY",
+								"expanded_url": "https://twitter.com/standardnews",
+								"display_url": "twitter.com/standardnews",
+								"indices": [
+									83,
+									106
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 133237,
+				"friends_count": 225,
+				"listed_count": 1324,
+				"created_at": "Mon Feb 11 10:02:16 +0000 2013",
+				"favourites_count": 126,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 185796,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1323213384540717056/neSs9b3h_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1323213384540717056/neSs9b3h_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1168472395/1565001946",
+				"profile_link_color": "DC062B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 107,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1016,
+		"favorite_count": 9723,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/CJcdqq9SGE https://t.co/LXhqMJLCZz"
+	},
+	{
 		"created_at": "Tue Jun 01 21:25:27 +0000 2021",
 		"id": 1399809229335273500,
 		"id_str": "1399839532837638144",
