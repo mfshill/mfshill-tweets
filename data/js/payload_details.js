daed7f45ad97dd5d4b74ac510d9d8e6998ed4c79
mfshill <mh-signup@outlook.com>
2021-06-25 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42057,
-	"created_at": "Thu Jun 24 01:17:37 +0000 2021",
+	"tweets": 42058,
+	"created_at": "Fri Jun 25 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 24 14:17:20 +0000 2021",
+		"id": 1408051890861060000,
+		"id_str": "1408066717197103117",
+		"full_text": "⚽ The away goals rule will be removed from all UEFA club competitions from the 2021/22 season.\n\nTies in which the two teams score the same number of goals over the two legs will now have two 15-minute periods of extra time, and, if required, penalty kicks.\n\n#UCL #UWCL #UEL #UYL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "UCL",
+					"indices": [
+						258,
+						262
+					]
+				},
+				{
+					"text": "UWCL",
+					"indices": [
+						263,
+						268
+					]
+				},
+				{
+					"text": "UEL",
+					"indices": [
+						269,
+						273
+					]
+				},
+				{
+					"text": "UYL",
+					"indices": [
+						274,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter for Advertisers.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2326857668,
+			"id_str": "2326857668",
+			"name": "UEFA",
+			"screen_name": "UEFA",
+			"location": "Nyon, Switzerland",
+			"description": "⚽️ The official home of UEFA on Twitter ⚽️ #EqualGame ▪️https://t.co/rJWMdsMbBW ▪️FB/UEFA ▪️IG/UEFA_official",
+			"url": "https://t.co/skKH3lwake",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/skKH3lwake",
+							"expanded_url": "http://uefa.com/insideuefa",
+							"display_url": "uefa.com/insideuefa",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/rJWMdsMbBW",
+							"expanded_url": "http://equalgame.com",
+							"display_url": "equalgame.com",
+							"indices": [
+								56,
+								79
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1678042,
+			"friends_count": 1065,
+			"listed_count": 3970,
+			"created_at": "Tue Feb 04 10:09:08 +0000 2014",
+			"favourites_count": 5629,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8006,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1407652277163376642/THZcSWDL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1407652277163376642/THZcSWDL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2326857668/1621932063",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5780,
+		"favorite_count": 17797,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "⚽ The away goals rule will be removed from all UEFA club competitions from the 2021/22 season.\n\nTies in which the two teams score the same number of goals over the two legs will now have two 15-minute periods of extra time, and, if required, penalty kicks.\n\n#UCL #UWCL #UEL #UYL"
+	},
+	{
 		"created_at": "Wed Jun 23 12:19:06 +0000 2021",
 		"id": 1406330220009996300,
 		"id_str": "1407674575513657345",
