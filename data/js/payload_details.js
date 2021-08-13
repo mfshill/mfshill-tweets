e46a13fc96653e4d6efab8148576e8dc44817740
mfshill <mh-signup@outlook.com>
2021-08-13 02:17:37 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42192,
-	"created_at": "Thu Aug 12 01:17:37 +0000 2021",
+	"tweets": 42193,
+	"created_at": "Fri Aug 13 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 12 18:36:31 +0000 2021",
+		"id": 1425848596465954800,
+		"id_str": "1425888948002430980",
+		"full_text": "'took Keith to the sexual health clinic this afternoon and got asked to leave due to Keith being a dog' is possibly one of the best collections of words any person has put together https://t.co/lXos5gfmqN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lXos5gfmqN",
+					"expanded_url": "https://twitter.com/oldenoughtosay/status/1425735702650753025",
+					"display_url": "twitter.com/oldenoughtosay…",
+					"indices": [
+						181,
+						204
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15397476,
+			"id_str": "15397476",
+			"name": "Chris Deeley",
+			"screen_name": "ThatChris1209",
+			"location": "London",
+			"description": "Soccer Editorial Director at @FanSided, bits @90min_football. Author of Forgotten Nations. vine mourner. PL/FIFA accredited. (chris.deeley@fansided.com)",
+			"url": "https://t.co/z3VqmPFmNR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/z3VqmPFmNR",
+							"expanded_url": "https://www.amazon.co.uk/Forgotten-Nations-Incredible-Stories-Football/dp/1785314564/",
+							"display_url": "amazon.co.uk/Forgotten-Nati…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2950,
+			"friends_count": 1006,
+			"listed_count": 49,
+			"created_at": "Fri Jul 11 22:14:11 +0000 2008",
+			"favourites_count": 18803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 114094,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1400391130265554947/hhEuG-h2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1400391130265554947/hhEuG-h2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15397476/1590187595",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1425735702650753000,
+		"quoted_status_id_str": "1425735702650753025",
+		"quoted_status_permalink": {
+			"url": "https://t.co/lXos5gfmqN",
+			"expanded": "https://twitter.com/oldenoughtosay/status/1425735702650753025",
+			"display": "twitter.com/oldenoughtosay…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Aug 12 08:27:35 +0000 2021",
+			"id": 1425735702650753000,
+			"id_str": "1425735702650753025",
+			"full_text": "Facebook is the gift that keeps giving https://t.co/WxaNlTd6dr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				38
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1425735696225120300,
+						"id_str": "1425735696225120256",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/E8k7D9UXsAAXW4v.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E8k7D9UXsAAXW4v.jpg",
+						"url": "https://t.co/WxaNlTd6dr",
+						"display_url": "pic.twitter.com/WxaNlTd6dr",
+						"expanded_url": "https://twitter.com/oldenoughtosay/status/1425735702650753025/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 470,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1626,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 830,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1425735696225120300,
+						"id_str": "1425735696225120256",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/E8k7D9UXsAAXW4v.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E8k7D9UXsAAXW4v.jpg",
+						"url": "https://t.co/WxaNlTd6dr",
+						"display_url": "pic.twitter.com/WxaNlTd6dr",
+						"expanded_url": "https://twitter.com/oldenoughtosay/status/1425735702650753025/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 470,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1125,
+								"h": 1626,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 830,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1425735696262865000,
+						"id_str": "1425735696262864897",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/E8k7D9dXoAEuDwZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/E8k7D9dXoAEuDwZ.jpg",
+						"url": "https://t.co/WxaNlTd6dr",
+						"display_url": "pic.twitter.com/WxaNlTd6dr",
+						"expanded_url": "https://twitter.com/oldenoughtosay/status/1425735702650753025/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 941,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1403,
+								"h": 1789,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 533,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1146793748,
+				"id_str": "1146793748",
+				"name": "madeline odent",
+				"screen_name": "oldenoughtosay",
+				"location": "London, England",
+				"description": "chaos feed. heritage, museums, old buildings, social justice. 🏴󠁧󠁢󠁷󠁬󠁳󠁿🇺🇸🏳️‍🌈. sea witch. i don't even endorse my own tweets. (she/her)",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11986,
+				"friends_count": 1567,
+				"listed_count": 77,
+				"created_at": "Mon Feb 04 02:11:26 +0000 2013",
+				"favourites_count": 99133,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 30656,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1289252026917040128/gUzNeJBN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1289252026917040128/gUzNeJBN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1146793748/1596748353",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 822,
+			"favorite_count": 5579,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 321,
+		"favorite_count": 1473,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'took Keith to the sexual health clinic this afternoon and got asked to leave due to Keith being a dog' is possibly one of the best collections of words any person has put together https://t.co/lXos5gfmqN"
+	},
+	{
 		"created_at": "Wed Aug 11 19:08:53 +0000 2021",
 		"id": 1425534703017373700,
 		"id_str": "1425534703017373704",
