faf9fcd3b0963abccdd78d23e9d166f3e043acfa
mfshill <mh-signup@outlook.com>
2021-09-02 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42236,
-	"created_at": "Mon Aug 30 01:17:38 +0000 2021",
+	"tweets": 42237,
+	"created_at": "Thu Sep 02 01:17:37 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_09.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Wed Sep 01 22:43:19 +0000 2021",
+		"id": 1433030603390193700,
+		"id_str": "1433198813339176960",
+		"full_text": "Actually the British left a lot of equipment and supplies behind at Dunkirk and it was used by the Germans. Farage is lying https://t.co/qdpbkzQpdZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1433030596842885000,
+					"id_str": "1433030596842885125",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MlvD0WEAU8ps5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MlvD0WEAU8ps5.jpg",
+					"url": "https://t.co/qdpbkzQpdZ",
+					"display_url": "pic.twitter.com/qdpbkzQpdZ",
+					"expanded_url": "https://twitter.com/DanKaszeta/status/1433030603390193668/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1149,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 639,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1149,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1433030596842885000,
+					"id_str": "1433030596842885125",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MlvD0WEAU8ps5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MlvD0WEAU8ps5.jpg",
+					"url": "https://t.co/qdpbkzQpdZ",
+					"display_url": "pic.twitter.com/qdpbkzQpdZ",
+					"expanded_url": "https://twitter.com/DanKaszeta/status/1433030603390193668/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1149,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 639,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1149,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 962069820,
+			"id_str": "962069820",
+			"name": "The Dan Kaszeta",
+			"screen_name": "DanKaszeta",
+			"location": "London, UK",
+			"description": "Assoc Fellow at RUSI. Security+CBRN. King of Nerve Agent Twitter. Ex US Army and USSS. Liveryman of London. 🇺🇸🇬🇧 dual citizen, ex-verger. Author of 'Toxic'",
+			"url": "https://t.co/V0LbvYqbPR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/V0LbvYqbPR",
+							"expanded_url": "https://uk.bookshop.org/shop/Kaszeta",
+							"display_url": "uk.bookshop.org/shop/Kaszeta",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22525,
+			"friends_count": 6343,
+			"listed_count": 393,
+			"created_at": "Wed Nov 21 09:26:44 +0000 2012",
+			"favourites_count": 32576,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 298034,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1229484802891567105/4BV06284_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1229484802891567105/4BV06284_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/962069820/1582630736",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1489,
+		"favorite_count": 6426,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Actually the British left a lot of equipment and supplies behind at Dunkirk and it was used by the Germans. Farage is lying https://t.co/qdpbkzQpdZ"
+	}
+]
