26418e538dc444fd766030b695a9ffe3955c64fd
mfshill <mh-signup@outlook.com>
2021-09-03 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42237,
-	"created_at": "Thu Sep 02 01:17:37 +0000 2021",
+	"tweets": 42238,
+	"created_at": "Fri Sep 03 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 02 20:02:29 +0000 2021",
+		"id": 1433520728071196700,
+		"id_str": "1433520728071196681",
+		"full_text": "Has anyone actually finished Ulysses? https://t.co/RmjDpd8Mw2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RmjDpd8Mw2",
+					"expanded_url": "https://twitter.com/BuddClair/status/1433512377312481287",
+					"display_url": "twitter.com/BuddClair/stat…",
+					"indices": [
+						38,
+						61
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 259,
+			"listed_count": 0,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1251,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 42115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1395702894452776962/1SfFeipp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1395702894452776962/1SfFeipp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1433512377312481300,
+		"quoted_status_id_str": "1433512377312481287",
+		"quoted_status_permalink": {
+			"url": "https://t.co/RmjDpd8Mw2",
+			"expanded": "https://twitter.com/BuddClair/status/1433512377312481287",
+			"display": "twitter.com/BuddClair/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 02 19:29:18 +0000 2021",
+			"id": 1433512377312481300,
+			"id_str": "1433512377312481287",
+			"full_text": "Jude the Obscure \nThe Road \nThe Carpetbaggers\nWolf Hall\nBastarding Ulysses (Except one day I *will* finish it. And hate every minute of it) https://t.co/IZrJleOOk1",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				139
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/IZrJleOOk1",
+						"expanded_url": "https://twitter.com/_F_B_G_/status/1433504005678895112",
+						"display_url": "twitter.com/_F_B_G_/status…",
+						"indices": [
+							140,
+							163
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1106867328921276400,
+				"id_str": "1106867328921276416",
+				"name": "Clair Budd",
+				"screen_name": "BuddClair",
+				"location": "North East England",
+				"description": "PhD student @sheffielduni, researching liminal personae as intercessory agents in the novels of Terry Pratchett and Neil Gaiman. 2nd wave feminist.",
+				"url": "https://t.co/opfKSJmsp1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/opfKSJmsp1",
+								"expanded_url": "https://clairbudd1.medium.com",
+								"display_url": "clairbudd1.medium.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 755,
+				"friends_count": 2153,
+				"listed_count": 5,
+				"created_at": "Sat Mar 16 10:38:30 +0000 2019",
+				"favourites_count": 71466,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 45727,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1428834389169999873/BPSLmYy5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1428834389169999873/BPSLmYy5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1106867328921276416/1591205234",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1433504005678895000,
+			"quoted_status_id_str": "1433504005678895112",
+			"quoted_status_permalink": {
+				"url": "https://t.co/IZrJleOOk1",
+				"expanded": "https://twitter.com/_F_B_G_/status/1433504005678895112",
+				"display": "twitter.com/_F_B_G_/status…"
+			},
+			"retweet_count": 1,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Has anyone actually finished Ulysses? https://t.co/RmjDpd8Mw2"
+	},
+	{
 		"created_at": "Wed Sep 01 22:43:19 +0000 2021",
 		"id": 1433030603390193700,
 		"id_str": "1433198813339176960",
