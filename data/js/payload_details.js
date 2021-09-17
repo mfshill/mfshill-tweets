492c972b91a6833e4c9c5a4349be00d9a204f727
mfshill <mh-signup@outlook.com>
2021-09-17 02:17:38 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42245,
-	"created_at": "Wed Sep 15 01:17:37 +0000 2021",
+	"tweets": 42247,
+	"created_at": "Fri Sep 17 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,272 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 16 21:46:34 +0000 2021",
+		"id": 1438610198042714000,
+		"id_str": "1438620349516689409",
+		"full_text": "As the government prepares to bring back imperial measures here’s your regular reminder of what Leave voters wanted to get out of Brexit. Read it and weep. https://t.co/4ObjUkvO0m",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1438610194561446000,
+					"id_str": "1438610194561445892",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E_b4WZEXMAQ0kRG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_b4WZEXMAQ0kRG.jpg",
+					"url": "https://t.co/4ObjUkvO0m",
+					"display_url": "pic.twitter.com/4ObjUkvO0m",
+					"expanded_url": "https://twitter.com/MarcDavenant/status/1438610198042714117/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 684,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 684,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438610194561446000,
+					"id_str": "1438610194561445892",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E_b4WZEXMAQ0kRG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_b4WZEXMAQ0kRG.jpg",
+					"url": "https://t.co/4ObjUkvO0m",
+					"display_url": "pic.twitter.com/4ObjUkvO0m",
+					"expanded_url": "https://twitter.com/MarcDavenant/status/1438610198042714117/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 684,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 684,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 350341283,
+			"id_str": "350341283",
+			"name": "Davenant 📸",
+			"screen_name": "MarcDavenant",
+			"location": "Earth",
+			"description": "Marc Davenant, documentary photographer, writer, wanderer, raconteur, citizen of the world, creator of the #GalleryofthePeople. Portrait of Britain winner 2020",
+			"url": "https://t.co/ea7PCEZbl1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ea7PCEZbl1",
+							"expanded_url": "https://davenantphotos.wordpress.com/portfolio-2/",
+							"display_url": "davenantphotos.wordpress.com/portfolio-2/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 46753,
+			"friends_count": 1736,
+			"listed_count": 626,
+			"created_at": "Sun Aug 07 16:24:22 +0000 2011",
+			"favourites_count": 164000,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 123096,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1383687706937487366/yfk2Mbc0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1383687706937487366/yfk2Mbc0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/350341283/1593903012",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26,
+		"favorite_count": 66,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "As the government prepares to bring back imperial measures here’s your regular reminder of what Leave voters wanted to get out of Brexit. Read it and weep. https://t.co/4ObjUkvO0m"
+	},
+	{
+		"created_at": "Thu Sep 16 18:18:29 +0000 2021",
+		"id": 1438567983811596300,
+		"id_str": "1438567983811596291",
+		"full_text": "fs 2-2 #RENTOT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RENTOT",
+					"indices": [
+						7,
+						14
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1438567201770987500,
+		"in_reply_to_status_id_str": "1438567201770987524",
+		"in_reply_to_user_id": 95424142,
+		"in_reply_to_user_id_str": "95424142",
+		"in_reply_to_screen_name": "mfshill",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259,
+			"friends_count": 260,
+			"listed_count": 22,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1252,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 42125,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1395702894452776962/1SfFeipp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1395702894452776962/1SfFeipp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "fs 2-2 #RENTOT"
+	},
+	{
 		"created_at": "Tue Sep 14 11:26:43 +0000 2021",
 		"id": 1437739583509340200,
 		"id_str": "1437739583509340161",
