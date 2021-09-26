55015b0d03ad9d0a67dc3404343f90b757f9d79f
mfshill <mh-signup@outlook.com>
2021-09-26 02:17:39 +0100

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 25 17:30:05 +0000 2021",
+		"id": 1441751804962910200,
+		"id_str": "1441817296738136065",
+		"full_text": "Today has been… https://t.co/wHT4WZUHdK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1441751799610974200,
+					"id_str": "1441751799610974208",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/FAIhn34XoAA3nV6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAIhn34XoAA3nV6.jpg",
+					"url": "https://t.co/wHT4WZUHdK",
+					"display_url": "pic.twitter.com/wHT4WZUHdK",
+					"expanded_url": "https://twitter.com/phi1mccann/status/1441751804962910208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1441751799610974200,
+					"id_str": "1441751799610974208",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/FAIhn34XoAA3nV6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAIhn34XoAA3nV6.jpg",
+					"url": "https://t.co/wHT4WZUHdK",
+					"display_url": "pic.twitter.com/wHT4WZUHdK",
+					"expanded_url": "https://twitter.com/phi1mccann/status/1441751804962910208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 40980586,
+			"id_str": "40980586",
+			"name": "Phil McCann",
+			"screen_name": "phi1mccann",
+			"location": "",
+			"description": "The BBC's Political Reporter for my home county of Cheshire. BBC North West Tonight, Politics NW, Radios Manchester, Merseyside & Stoke. RTs not endorsements.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9600,
+			"friends_count": 1183,
+			"listed_count": 87,
+			"created_at": "Mon May 18 22:02:44 +0000 2009",
+			"favourites_count": 783,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12465,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1908641603/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1908641603/image_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/40980586/1555591288",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2006,
+		"favorite_count": 50229,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today has been… https://t.co/wHT4WZUHdK"
+	},
+	{
 		"created_at": "Fri Sep 24 19:09:56 +0000 2021",
 		"id": 1441479335848808400,
 		"id_str": "1441480035039268873",
