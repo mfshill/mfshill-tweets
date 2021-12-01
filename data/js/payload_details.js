e564fe99d41c803116a55a1b24178f5bbbf0af95
mfshill <mh-signup@outlook.com>
2021-12-01 01:17:38 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42433,
-	"created_at": "Tue Nov 30 01:17:38 +0000 2021",
+	"tweets": 42434,
+	"created_at": "Wed Dec 01 01:17:37 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 30 20:39:17 +0000 2021",
+		"id": 1465676413579710500,
+		"id_str": "1465782509694824448",
+		"full_text": "November 30, 1969: the Muppets appear on The Ed Sullivan Show and perform “Mah Na Mah Na”, which had been done on Sesame Street only 3 days prior\n\nThis version featured pink creatures known as Snowths, and would serve as the basis for all future Muppet versions of the song https://t.co/2yGaWIBxGT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1465675765027061800,
+					"id_str": "1465675765027061763",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1465675765027061763/pu/img/COW5umciMnx7mI3h.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1465675765027061763/pu/img/COW5umciMnx7mI3h.jpg",
+					"url": "https://t.co/2yGaWIBxGT",
+					"display_url": "pic.twitter.com/2yGaWIBxGT",
+					"expanded_url": "https://twitter.com/HistoryMuppet/status/1465676413579710464/video/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1465675765027061800,
+					"id_str": "1465675765027061763",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1465675765027061763/pu/img/COW5umciMnx7mI3h.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1465675765027061763/pu/img/COW5umciMnx7mI3h.jpg",
+					"url": "https://t.co/2yGaWIBxGT",
+					"display_url": "pic.twitter.com/2yGaWIBxGT",
+					"expanded_url": "https://twitter.com/HistoryMuppet/status/1465676413579710464/video/1",
+					"type": "video",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 140000,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465675765027061763/pu/vid/480x270/7C7XozNd84pA8v_i.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465675765027061763/pu/vid/1024x576/NtjApaUxTgHMX5y9.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465675765027061763/pu/vid/640x360/FQhGESWNfFo7zUYP.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1465675765027061763/pu/pl/NXBMIwqwi83m_0DC.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1049365727705288700,
+			"id_str": "1049365727705288704",
+			"name": "⛄️ Muppet History ❄️",
+			"screen_name": "HistoryMuppet",
+			"location": "",
+			"description": "A fan page dedicated to continuing the spirit and silliness of Jim Henson! (He/Him) Created and Operated by @HalfHearted_JG Business: MuppetHistory@outlook.com",
+			"url": "https://t.co/Cs6QgBY0vH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Cs6QgBY0vH",
+							"expanded_url": "https://linktr.ee/MuppetHistory",
+							"display_url": "linktr.ee/MuppetHistory",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 218054,
+			"friends_count": 1591,
+			"listed_count": 762,
+			"created_at": "Mon Oct 08 18:27:40 +0000 2018",
+			"favourites_count": 49675,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21889,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1459694384727871488/H9c7-PDJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1459694384727871488/H9c7-PDJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1049365727705288704/1637938794",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1325,
+		"favorite_count": 5155,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "November 30, 1969: the Muppets appear on The Ed Sullivan Show and perform “Mah Na Mah Na”, which had been done on Sesame Street only 3 days prior\n\nThis version featured pink creatures known as Snowths, and would serve as the basis for all future Muppet versions of the song https://t.co/2yGaWIBxGT"
+	},
+	{
 		"created_at": "Mon Nov 29 17:13:34 +0000 2021",
 		"id": 1465368348170997800,
 		"id_str": "1465368348170997761",
