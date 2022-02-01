3721d8e143f3fe8598a23e1d4d3c57911e625cfd
mfshill <mh-signup@outlook.com>
2022-02-01 01:17:39 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42592,
-	"created_at": "Mon Jan 31 01:17:38 +0000 2022",
+	"tweets": 42593,
+	"created_at": "Tue Feb 01 01:17:38 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,189 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 01 00:04:33 +0000 2022",
+		"id": 1488302214271623200,
+		"id_str": "1488302214271623172",
+		"full_text": "@TC_Cornesto get 'em whilst you can! https://t.co/vVUkU3nCEq",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TC_Cornesto",
+					"name": "TC Cornesto",
+					"id": 19991214,
+					"id_str": "19991214",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1488302207455932400,
+					"id_str": "1488302207455932421",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKeC-OnXMAUBBdx.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKeC-OnXMAUBBdx.jpg",
+					"url": "https://t.co/vVUkU3nCEq",
+					"display_url": "pic.twitter.com/vVUkU3nCEq",
+					"expanded_url": "https://twitter.com/mfshill/status/1488302214271623172/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488302207455932400,
+					"id_str": "1488302207455932421",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKeC-OnXMAUBBdx.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKeC-OnXMAUBBdx.jpg",
+					"url": "https://t.co/vVUkU3nCEq",
+					"display_url": "pic.twitter.com/vVUkU3nCEq",
+					"expanded_url": "https://twitter.com/mfshill/status/1488302214271623172/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 398,
+							"h": 280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							199,
+							140
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FKeC-OnXMAUBBdx.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1488301739866529800,
+		"in_reply_to_status_id_str": "1488301739866529800",
+		"in_reply_to_user_id": 19991214,
+		"in_reply_to_user_id_str": "19991214",
+		"in_reply_to_screen_name": "TC_Cornesto",
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 252,
+			"listed_count": 22,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1308,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 42538,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1484231894048903180/TeSazBL8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1484231894048903180/TeSazBL8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@TC_Cornesto get 'em whilst you can! https://t.co/vVUkU3nCEq"
+	}
+]
