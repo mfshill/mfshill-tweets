c43e520d52f674cfded412392a02f3717f886af1
mfshill <mh-signup@outlook.com>
2022-05-06 02:17:38 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43165,
-	"created_at": "Thu May 05 01:17:37 +0000 2022",
+	"tweets": 43166,
+	"created_at": "Fri May 06 01:17:37 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 05 14:23:22 +0000 2022",
+		"id": 1522220413023006700,
+		"id_str": "1522220413023006720",
+		"full_text": "top shelf moron, if he doesn't know the basics of road signage, probably shouldn't be driving a vehicle. https://t.co/Xzi3Ntk3eT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Xzi3Ntk3eT",
+					"expanded_url": "https://twitter.com/thatdanstewart/status/1522168889286791174",
+					"display_url": "twitter.com/thatdanstewart…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257,
+			"friends_count": 260,
+			"listed_count": 21,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1351,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 43190,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1515349711426834434/1yrKoKZN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1515349711426834434/1yrKoKZN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1522168889286791200,
+		"quoted_status_id_str": "1522168889286791174",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Xzi3Ntk3eT",
+			"expanded": "https://twitter.com/thatdanstewart/status/1522168889286791174",
+			"display": "twitter.com/thatdanstewart…"
+		},
+		"quoted_status": {
+			"created_at": "Thu May 05 10:58:38 +0000 2022",
+			"id": 1522168889286791200,
+			"id_str": "1522168889286791174",
+			"full_text": "Question to UK followers: do you know what this sign means? I didn't, but thankfully I just received a helpful reminder from Enfield Council -- and it only cost me £65 https://t.co/GnoTR96o8l",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				167
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1522168852108521500,
+						"id_str": "1522168852108521472",
+						"indices": [
+							168,
+							191
+						],
+						"media_url": "http://pbs.twimg.com/media/FR_UgZYXsAA7SWU.png",
+						"media_url_https": "https://pbs.twimg.com/media/FR_UgZYXsAA7SWU.png",
+						"url": "https://t.co/GnoTR96o8l",
+						"display_url": "pic.twitter.com/GnoTR96o8l",
+						"expanded_url": "https://twitter.com/thatdanstewart/status/1522168889286791174/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1522168852108521500,
+						"id_str": "1522168852108521472",
+						"indices": [
+							168,
+							191
+						],
+						"media_url": "http://pbs.twimg.com/media/FR_UgZYXsAA7SWU.png",
+						"media_url_https": "https://pbs.twimg.com/media/FR_UgZYXsAA7SWU.png",
+						"url": "https://t.co/GnoTR96o8l",
+						"display_url": "pic.twitter.com/GnoTR96o8l",
+						"expanded_url": "https://twitter.com/thatdanstewart/status/1522168889286791174/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 589,
+								"h": 348,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 82391708,
+				"id_str": "82391708",
+				"name": "Dan Stewart",
+				"screen_name": "thatdanstewart",
+				"location": "London, England",
+				"description": "Longform editor, FT. Formerly TIME, The Week. Londoner, but spent time as a New Yorker. Email me at dan dot stewart at ft dot com",
+				"url": "https://t.co/5BruVGQoz9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5BruVGQoz9",
+								"expanded_url": "http://www.ft.com",
+								"display_url": "ft.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4716,
+				"friends_count": 2136,
+				"listed_count": 133,
+				"created_at": "Wed Oct 14 15:57:50 +0000 2009",
+				"favourites_count": 2153,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 10491,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1243160520498315266/CTUNtvKQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1243160520498315266/CTUNtvKQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/82391708/1641955337",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 123,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "top shelf moron, if he doesn't know the basics of road signage, probably shouldn't be driving a vehicle. https://t.co/Xzi3Ntk3eT"
+	},
+	{
 		"created_at": "Wed May 04 19:56:45 +0000 2022",
 		"id": 1521941923581337600,
 		"id_str": "1521941923581337602",
