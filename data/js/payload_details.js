870258bbc2a73e82665741558a38af0a4716cdd5
mfshill <mh-signup@outlook.com>
2022-06-11 02:17:37 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43304,
-	"created_at": "Fri Jun 10 01:17:37 +0000 2022",
+	"tweets": 43305,
+	"created_at": "Sat Jun 11 01:17:37 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 10 20:33:11 +0000 2022",
+		"id": 1535359443055026200,
+		"id_str": "1535359443055026178",
+		"full_text": "https://t.co/kORrXYg8Yk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1535359433513083000,
+					"id_str": "1535359433513082880",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FU6xRSUXoAAz4C1.png",
+					"media_url_https": "https://pbs.twimg.com/media/FU6xRSUXoAAz4C1.png",
+					"url": "https://t.co/kORrXYg8Yk",
+					"display_url": "pic.twitter.com/kORrXYg8Yk",
+					"expanded_url": "https://twitter.com/mfshill/status/1535359443055026178/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 468,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 265,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 1230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1535359433513083000,
+					"id_str": "1535359433513082880",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FU6xRSUXoAAz4C1.png",
+					"media_url_https": "https://pbs.twimg.com/media/FU6xRSUXoAAz4C1.png",
+					"url": "https://t.co/kORrXYg8Yk",
+					"display_url": "pic.twitter.com/kORrXYg8Yk",
+					"expanded_url": "https://twitter.com/mfshill/status/1535359443055026178/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 468,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 265,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 1230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 262,
+			"listed_count": 21,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1342,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 43271,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1515349711426834434/1yrKoKZN_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1515349711426834434/1yrKoKZN_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/kORrXYg8Yk"
+	},
+	{
 		"created_at": "Thu Jun 09 20:21:00 +0000 2022",
 		"id": 1534993988410916900,
 		"id_str": "1534993988410916893",
