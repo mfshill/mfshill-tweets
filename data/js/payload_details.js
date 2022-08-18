3f414e5a539ad437c9b85ba292a42c091589a7da
mfshill <mh-signup@outlook.com>
2022-08-18 02:17:39 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43546,
-	"created_at": "Wed Aug 17 01:17:38 +0000 2022",
+	"tweets": 43547,
+	"created_at": "Thu Aug 18 01:17:38 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 17 17:35:22 +0000 2022",
+		"id": 1559957068475506700,
+		"id_str": "1559957068475506688",
+		"full_text": "I'm not going to ask #drybumming https://t.co/0Rka9ejMHi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "drybumming",
+					"indices": [
+						21,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1559956988099952600,
+					"id_str": "1559956988099952641",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FaYUnZyWAAE2sGw.png",
+					"media_url_https": "https://pbs.twimg.com/media/FaYUnZyWAAE2sGw.png",
+					"url": "https://t.co/0Rka9ejMHi",
+					"display_url": "pic.twitter.com/0Rka9ejMHi",
+					"expanded_url": "https://twitter.com/mfshill/status/1559957068475506688/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 82,
+							"h": 82,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1559956988099952600,
+					"id_str": "1559956988099952641",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/FaYUnZyWAAE2sGw.png",
+					"media_url_https": "https://pbs.twimg.com/media/FaYUnZyWAAE2sGw.png",
+					"url": "https://t.co/0Rka9ejMHi",
+					"display_url": "pic.twitter.com/0Rka9ejMHi",
+					"expanded_url": "https://twitter.com/mfshill/status/1559957068475506688/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 82,
+							"h": 82,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 195,
+							"h": 82,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 265,
+			"listed_count": 21,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1378,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 43538,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1541909736030969858/cJoSXXWu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1541909736030969858/cJoSXXWu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm not going to ask #drybumming https://t.co/0Rka9ejMHi"
+	},
+	{
 		"created_at": "Tue Aug 16 21:12:42 +0000 2022",
 		"id": 1559649376708038700,
 		"id_str": "1559649376708038656",
