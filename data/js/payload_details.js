40683e58c6f283a2a71ee35336a8b9bff507de3d
mfshill <mh-signup@outlook.com>
2022-09-02 02:17:39 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43596,
-	"created_at": "Thu Sep 01 01:17:38 +0000 2022",
+	"tweets": 43597,
+	"created_at": "Fri Sep 02 01:17:38 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_09.js
@@ -0,0 +1,278 @@
+Grailbird.data.tweets_2022_09 = 
+[
+	{
+		"created_at": "Thu Sep 01 18:53:47 +0000 2022",
+		"id": 1565412620596703200,
+		"id_str": "1565412620596703232",
+		"full_text": "“Dearly beloved, we are gathered here today to get through this thing called Life” https://t.co/UXiWFJPizw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UXiWFJPizw",
+					"expanded_url": "https://twitter.com/AntRDean/status/1564944321790545921",
+					"display_url": "twitter.com/AntRDean/statu…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 95424142,
+			"id_str": "95424142",
+			"name": "Hilly",
+			"screen_name": "mfshill",
+			"location": "Null Island",
+			"description": "\"Força, Equilibri, Valor y Seny\" (Strength, balance, courage & common sense)\n\"vei lomani\" (work together; love one another)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 254,
+			"friends_count": 265,
+			"listed_count": 21,
+			"created_at": "Tue Dec 08 13:51:57 +0000 2009",
+			"favourites_count": 1386,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 43591,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1541909736030969858/cJoSXXWu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1541909736030969858/cJoSXXWu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/95424142/1590002835",
+			"profile_link_color": "9D6238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EDE1B0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1564944321790546000,
+		"quoted_status_id_str": "1564944321790545921",
+		"quoted_status_permalink": {
+			"url": "https://t.co/UXiWFJPizw",
+			"expanded": "https://twitter.com/AntRDean/status/1564944321790545921",
+			"display": "twitter.com/AntRDean/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Aug 31 11:52:56 +0000 2022",
+			"id": 1564944321790546000,
+			"id_str": "1564944321790545921",
+			"full_text": "https://t.co/9Dad2VXmvf",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				0
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1564944316446957600,
+						"id_str": "1564944316446957568",
+						"indices": [
+							0,
+							23
+						],
+						"media_url": "http://pbs.twimg.com/media/FbfMkHsWYAAJSXn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FbfMkHsWYAAJSXn.jpg",
+						"url": "https://t.co/9Dad2VXmvf",
+						"display_url": "pic.twitter.com/9Dad2VXmvf",
+						"expanded_url": "https://twitter.com/AntRDean/status/1564944321790545921/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1452,
+								"h": 1936,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1564944316446957600,
+						"id_str": "1564944316446957568",
+						"indices": [
+							0,
+							23
+						],
+						"media_url": "http://pbs.twimg.com/media/FbfMkHsWYAAJSXn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FbfMkHsWYAAJSXn.jpg",
+						"url": "https://t.co/9Dad2VXmvf",
+						"display_url": "pic.twitter.com/9Dad2VXmvf",
+						"expanded_url": "https://twitter.com/AntRDean/status/1564944321790545921/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1452,
+								"h": 1936,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 228825520,
+				"id_str": "228825520",
+				"name": "Ant, Lord Dean",
+				"screen_name": "AntRDean",
+				"location": "Twitter.",
+				"description": "Bins out on a Monday night? I’m your kind of guy. Declared to be ‘really pleasant’ by a piss doctor. I used to write music. Prolific procrastinator.",
+				"url": "https://t.co/ybgHGFvd5D",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ybgHGFvd5D",
+								"expanded_url": "https://www.amazon.co.uk/hz/wishlist/ls/3JIGULLFBJA6P?ref_=wl_share",
+								"display_url": "amazon.co.uk/hz/wishlist/ls…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2727,
+				"friends_count": 3480,
+				"listed_count": 34,
+				"created_at": "Mon Dec 20 19:46:44 +0000 2010",
+				"favourites_count": 54033,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 86868,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1535972256026877953/-FZPqdPj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1535972256026877953/-FZPqdPj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/228825520/1537615804",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 57,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "zxx"
+		},
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Dearly beloved, we are gathered here today to get through this thing called Life” https://t.co/UXiWFJPizw"
+	}
+]
